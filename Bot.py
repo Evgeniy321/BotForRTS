ae709850f136771bc84c0e90a69956f643671152
FsaMudi <98121431+FsaMudi@users.noreply.github.com>
2022-05-17 15:39:37 +0300

    bot.py
    Сделал совсем другой код

--- Bot.py
+++ Bot.py
@@ -1,37 +1,62 @@
 import discord
 from discord.ext import commands
-from config import settings
-
-
-
-
-bot = commands.Bot(command_prefix = settings['prefix']) #Префикс в config.py
-    
-    
-    
-@bot.command() 
-async def start(ctx): 
-    file1 = open('1question.txt', encoding='utf-8')
-    file2 = open('2question.txt', encoding='utf-8')
-    file3 = open('3question.txt', encoding='utf-8')
-    file4 = open('4question.txt', encoding='utf-8')
-    file5 = open('5question.txt', encoding='utf-8')
-    read1 = file1.read()
-    read2 = file2.read()
-    read3 = file3.read()
-    read4 = file4.read()
-    read5 = file5.read()
-    await ctx.send(read1)
-    await ctx.send(read2)
-    await ctx.send(read3)
-    await ctx.send(read4)
-    await ctx.send(read5)
-    
+import requests
+import asyncio
+import time
 
 
 
 
 
+DISCORD_BOT_TOKEN = '' #токен
 
-bot.run(settings['token'])
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_voice_state_update(message, before, after,):
+    if before.channel is None and after.channel is not None:
+        channel = client.get_channel(975044149189435472)
+        await channel.send(embed = discord.Embed(description = f'Посмотрите консоль!', colour = discord.Color.green()))
+        print('Введите айди канала')
+        channelid = input()
+        chid = int(channelid)
+    elif before.channel is not None and after.channel is None:
+        channel1 = client.get_channel(975044149189435472)
+        await channel1.send(embed = discord.Embed(description = f'1. Как тебе сегодняшний урок?', colour = discord.Color.green()))
+        await channel1.send(embed = discord.Embed(description = f'**Подсказка**', colour = discord.Color.green()))
+        await channel1.send(embed = discord.Embed(description = f'Что-бы ответить пиши 1.(Ответ)', colour = discord.Color.green()))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('1.'):
+        seconds = time.time()
+        date = time.ctime(seconds)
+        member = message.author
+        mcontent = message.content
+        channel = client.get_channel(975044149189435472)
+        logchannel = client.get_channel(975118850947444816)
+        await message.send(embed = discord.Embed(description = f'Спасибо за оценку!', colour = discord.Color.green()))
+        await logchannel.send(embed = discord.Embed(description = f'+1 Log', colour = discord.Color.green()))
+        await logchannel.send(embed = discord.Embed(description = f'Человек: {member.mention}', colour = discord.Color.green()))
+        await logchannel.send(embed = discord.Embed(description = f'``Сообщение: {mcontent}``', colour = discord.Color.green()))
+        await logchannel.send(embed = discord.Embed(description = f'``Дата: {date}``', colour = discord.Color.green()))
+
+
+
+
+
+
+
+
+
+client.run(DISCORD_BOT_TOKEN)
+
 
